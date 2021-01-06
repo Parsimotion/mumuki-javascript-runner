6a71e078bdef0030c9d522279410745df0420203
Gaston Prieto <gaston@producteca.com>
2021-01-06 20:14:53 -0300

    extract docker pull

---
+++ docker-pull.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+JAVASCRIPT_WORKER_VERSION=${JAVASCRIPT_WORKER_VERSION:-productecadev/mumuki-mocha-worker-node12:1.0}
+docker pull $JAVASCRIPT_WORKER_VERSION 
--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #/bin/sh
 
-JAVASCRIPT_WORKER_VERSION=${JAVASCRIPT_WORKER_VERSION:-productecadev/mumuki-mocha-worker-node12:1.0}
-
-docker pull $JAVASCRIPT_WORKER_VERSION && \
+./docker-pull.sh && \
     bundle exec rackup --host "0.0.0.0" -p 9000
